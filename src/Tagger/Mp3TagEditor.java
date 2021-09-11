345153f7312ee3643a9dca514f3a6be65466a5e4
madebyTimo <t.schnaible@madebytimo.de>
2021-09-11 22:35:42 +0200

    edit exception handling

--- src/Tagger/Mp3TagEditor.java
+++ src/Tagger/Mp3TagEditor.java
@@ -10,25 +10,33 @@ import org.farng.mp3.id3.ID3v1;
 public class Mp3TagEditor {
 
 	private MP3File song;
-	
-	public Mp3TagEditor() {		
+
+	public Mp3TagEditor() {
 	}
-	
-	public Mp3Tags readTags(File pSong) throws IOException, TagException {
-		song = new MP3File(pSong);
-		ID3v1 tag = song.getID3v1Tag();
-		String artist = tag.getArtist();
-		String title = tag.getSongTitle();
-		Mp3Tags tags = new Mp3Tags(title, artist);
-		
+
+	public Mp3Tags readTags(File pSong) {
+		Mp3Tags tags = new Mp3Tags("unknown", "unknown");
+		try {
+			song = new MP3File(pSong);
+			ID3v1 tag = song.getID3v1Tag();
+			String artist = tag.getArtist();
+			String title = tag.getSongTitle();
+			tags = new Mp3Tags(title, artist);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return tags;
 	}
-	
-	public void writeTags(File pSong, Mp3Tags tags) throws IOException, TagException {
-		song = new MP3File(pSong);
-		ID3v1 tag = song.getID3v1Tag();
-		tag.setSongTitle(tags.getTitle());
-		tag.setArtist(tags.getArtist());
-		song.setID3v1Tag(tag);
+
+	public void writeTags(File pSong, Mp3Tags tags) {
+		try {
+			song = new MP3File(pSong);
+			ID3v1 tag = song.getID3v1Tag();
+			tag.setSongTitle(tags.getTitle());
+			tag.setArtist(tags.getArtist());
+			song.setID3v1Tag(tag);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }

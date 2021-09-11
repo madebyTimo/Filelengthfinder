44c7e19d5c770681eb060333f28fe342b0526dea
dennis12493 <49130130+dennis12493@users.noreply.github.com>
2021-09-11 21:51:05 +0200

    TagEditor read/write

--- src/Tagger/Mp3TagEditor.java
+++ src/Tagger/Mp3TagEditor.java
@@ -1,18 +1,36 @@
 package Tagger;
 
 import java.io.File;
+import java.io.IOException;
+
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagException;
+import org.farng.mp3.id3.ID3v1;
 
 public class Mp3TagEditor {
 
-	private File song;
+	private MP3File song;
 	
 	public Mp3TagEditor(File pSong) {
-		song = pSong;
+		try {
+			song = new MP3File(pSong);
+		}catch (IOException | TagException e) {
+			e.printStackTrace();
+		}		
 	}
 	
-	public void readTags() {
-		if(!song.isDirectory()) {
-			
-		}
+	public Mp3Tags readTags() {
+		ID3v1 tag = song.getID3v1Tag();
+		String artist = tag.getArtist();
+		String title = tag.getSongTitle();
+		Mp3Tags tags = new Mp3Tags(title, artist);
+		
+		return tags;
+	}
+	
+	public void writeTags(Mp3Tags tags) {
+		ID3v1 tag = song.getID3v1Tag();
+		tag.setSongTitle(tags.getTitle());
+		tag.setArtist(tags.getArtist());
 	}
 }

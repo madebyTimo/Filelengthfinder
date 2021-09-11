682c4aa947b5a6103f8495f57b45718f0ca1fc5e
madebyTimo <t.schnaible@madebytimo.de>
2021-09-11 23:18:05 +0200

    remove jid3lib

--- src/Tagger/Mp3TagEditor.java
+++ src/Tagger/Mp3TagEditor.java
@@ -1,42 +1,20 @@
 package Tagger;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.farng.mp3.MP3File;
-import org.farng.mp3.TagException;
-import org.farng.mp3.id3.ID3v1;
 
 public class Mp3TagEditor {
 
-	private MP3File song;
 
 	public Mp3TagEditor() {
+		
 	}
 
 	public Mp3Tags readTags(File pSong) {
-		Mp3Tags tags = new Mp3Tags("unknown", "unknown");
-		try {
-			song = new MP3File(pSong);
-			ID3v1 tag = song.getID3v1Tag();
-			String artist = tag.getArtist();
-			String title = tag.getSongTitle();
-			tags = new Mp3Tags(title, artist);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return tags;
+		return null;
 	}
 
 	public void writeTags(File pSong, Mp3Tags tags) {
-		try {
-			song = new MP3File(pSong);
-			ID3v1 tag = song.getID3v1Tag();
-			tag.setSongTitle(tags.getTitle());
-			tag.setArtist(tags.getArtist());
-			song.setID3v1Tag(tag);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
 	}
 }
